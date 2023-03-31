19b6bacbcf669620a7be4a2be5d3f420fbc8cc96
Efnilite <35348263+Efnilite@users.noreply.github.com>
2023-03-31 15:07:32 +0200

    Made pasting use async for block getting and sync for block setting

--- witp/src/main/java/dev/efnilite/ip/schematic/v2/io/SchematicPaster.java
+++ witp/src/main/java/dev/efnilite/ip/schematic/v2/io/SchematicPaster.java
@@ -2,13 +2,18 @@ package dev.efnilite.ip.schematic.v2.io;
 
 import dev.efnilite.ip.IP;
 import dev.efnilite.ip.util.Colls;
-import dev.efnilite.vilib.util.Task;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
@@ -20,18 +25,12 @@ public class SchematicPaster {
      *
      * @param location      The smallest location.
      * @param vectorDataMap The map.
+     * @return The affected blocks.
      */
-    public void paste(Location location, Map<Vector, BlockData> vectorDataMap) {
-        Runnable runnable = () -> vectorDataMap.forEach((vector, blockData) -> location.clone()
-                .add(vector)
-                .getBlock()
-                .setBlockData(blockData, false));
-
-        if (Bukkit.isPrimaryThread()) {
-            runnable.run();
-        } else {
-            Task.create(IP.getPlugin()).execute(runnable).run();
-        }
+    public List<Block> paste(Location location, Map<Vector, BlockData> vectorDataMap) {
+        return paste(() -> Colls.thread(vectorDataMap)
+                .mapk((k, v) -> location.clone().add(k).getBlock())
+                .get());
     }
 
     /**
@@ -40,23 +39,24 @@ public class SchematicPaster {
      * @param location      The smallest location.
      * @param rotation      The rotation.
      * @param vectorDataMap The map.
+     * @return The affected blocks.
      */
-    public void paste(Location location, Vector rotation, Map<Vector, BlockData> vectorDataMap) {
-        double[] constants = getRotationConstants(rotation);
+    public List<Block> paste(Location location, Vector rotation, Map<Vector, BlockData> vectorDataMap) {
+        return paste(() -> {
+            double[] constants = getRotationConstants(rotation);
 
-        Map<Vector, BlockData> newMap = Colls.mapk((vector, v) -> {
-            double x = vector.getX();
-            double y = vector.getY();
-            double z = vector.getZ();
+            return Colls.thread(vectorDataMap).mapk((vector, v) -> {
+                double x = vector.getX();
+                double y = vector.getY();
+                double z = vector.getZ();
 
-            double newX = constants[0] * x + constants[1] * y + constants[2] * z;
-            double newY = constants[3] * x + constants[4] * y + constants[5] * z;
-            double newZ = constants[6] * x + constants[7] * y + constants[8] * z;
+                double newX = constants[0] * x + constants[1] * y + constants[2] * z;
+                double newY = constants[3] * x + constants[4] * y + constants[5] * z;
+                double newZ = constants[6] * x + constants[7] * y + constants[8] * z;
 
-            return new Vector(newX, newY, newZ);
-        }, vectorDataMap);
-
-        paste(location, newMap);
+                return location.clone().add(newX, newY, newZ).getBlock();
+            }).get();
+        });
     }
 
     // 3d rotation matrix constants, calculate once instead of however many blocks we have times
@@ -66,6 +66,23 @@ public class SchematicPaster {
         double y = rotation.getZ();
 
         // src https://en.wikipedia.org/wiki/Rotation_matrix#General_rotations
-        return new double[]{cos(a) * cos(b), cos(a) * sin(b) * sin(y) - sin(a) * cos(y), cos(a) * sin(b) * cos(y) + sin(a) * sin(y), sin(a) * cos(b), sin(a) * sin(b) * sin(y) + cos(a) * cos(y), sin(a) * sin(b) * cos(y) - cos(a) * sin(y), -sin(b), cos(b) * sin(y), cos(b) * cos(y)};
+        return new double[] {
+                cos(a) * cos(b),    cos(a) * sin(b) * sin(y) - sin(a) * cos(y),     cos(a) * sin(b) * cos(y) + sin(a) * sin(y),
+                sin(a) * cos(b),    sin(a) * sin(b) * sin(y) + cos(a) * cos(y),     sin(a) * sin(b) * cos(y) - cos(a) * sin(y),
+                -sin(b),            cos(b) * sin(y),                                cos(b) * cos(y)};
+    }
+
+    private List<Block> paste(Supplier<Map<Block, BlockData>> blocksGetter) {
+        try {
+            Map<Block, BlockData> blocks = CompletableFuture.supplyAsync(blocksGetter).get();
+
+            blocks.forEach((block, data) -> block.setBlockData(data, false));
+
+            return new ArrayList<>(blocks.keySet());
+        } catch (InterruptedException | ExecutionException ex) {
+            IP.logging().stack("Error while trying to get blocks of schematic", ex);
+
+            return Collections.emptyList();
+        }
     }
 }
