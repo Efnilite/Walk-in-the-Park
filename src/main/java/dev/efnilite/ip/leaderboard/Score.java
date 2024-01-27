bef4e63c9afb8ad948440cb64ba8eb698055d244
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-01-27 21:15:48 +0100

    Update permission in config

--- src/main/java/dev/efnilite/ip/leaderboard/Score.java
+++ src/main/java/dev/efnilite/ip/leaderboard/Score.java
@@ -1,7 +1,5 @@
 package dev.efnilite.ip.leaderboard;
 
-import dev.efnilite.vilib.util.Time;
-
 /**
  * Represents a record, used to keep track of the score a player may achieve.
  *
@@ -21,52 +19,7 @@ public record Score(String name, String time, String difficulty, int score) {
     public static Score fromString(String string) {
         String[] parts = string.split(",");
 
-        return new Score(parts[0], parseV1Score(parts[1]), parts[2], Integer.parseInt(parts[3]));
-    }
-
-    /**
-     * Legacy migration.
-     * @param old The old score format.
-     * @return The new score format.
-     */
-    private static String parseV1Score(String old) {
-        if (old.contains(":")) {
-            return old;
-        }
-
-        double totalSec = 0; // total duration in ms
-
-        for (String part : old.trim().split(" ")) {
-            if (part.contains("h")) {
-                totalSec += Integer.parseInt(part.replace("h", "")) * Time.SECONDS_PER_HOUR;
-            } else if (part.contains("m")) {
-                totalSec += Integer.parseInt(part.replace("m", "")) * Time.SECONDS_PER_MINUTE;
-            } else if (part.contains("s")) {
-                totalSec += Double.parseDouble(part.replace("s", ""));
-            }
-        }
-
-        return timeFromMillis((int) (totalSec * 1000));
-    }
-
-    /**
-     * @param ms The duration in millis.
-     * @return The formatted time.
-     */
-    public static String timeFromMillis(int ms) {
-        int m = ms / (60 * 1000);
-        ms -= (m * 60 * 1000);
-
-        int s = ms / 1000;
-        ms -= (s * 1000);
-
-        return "%s:%s:%s".formatted(padLeft(Integer.toString(m), (m < 10) ? 1 : 0),
-                padLeft(Integer.toString(s), (s < 10) ? 1 : 0),
-                padLeft(Integer.toString(ms), (ms < 100) ? 2 : 0));
-    }
-
-    private static String padLeft(String s, int extraZeroes) {
-        return extraZeroes > 0 ? String.format("%" + (extraZeroes + 1) + "s", s).replace(" ", "0") : s;
+        return new Score(parts[0], parts[1], parts[2], Integer.parseInt(parts[3]));
     }
 
     /**
